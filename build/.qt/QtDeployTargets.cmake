136d25a0b8b586648aa837408d26a8bdf4c137ae
Ritik Raj <ritikraj@Ritiks-MacBook-Air-2.local>
2026-04-01 14:14:04 +0530

    Fix CMakeLists.txt include paths and build configuration

---
+++ build/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_MicroVectorEditor_FILE /Users/ritikraj/Desktop/Micro-Vector-Editor/build/MicroVectorEditor.app/Contents/MacOS/MicroVectorEditor)
+set(__QT_DEPLOY_TARGET_MicroVectorEditor_TYPE EXECUTABLE)
---
+++ build/CMakeFiles/MicroVectorEditor.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/build/MicroVectorEditor_autogen/mocs_compilation.cpp" "CMakeFiles/MicroVectorEditor.dir/MicroVectorEditor_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/MicroVectorEditor_autogen/mocs_compilation.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/Canvas.cpp" "CMakeFiles/MicroVectorEditor.dir/src/Canvas.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/Canvas.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/Circle.cpp" "CMakeFiles/MicroVectorEditor.dir/src/Circle.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/Circle.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/FreeHand.cpp" "CMakeFiles/MicroVectorEditor.dir/src/FreeHand.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/FreeHand.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/Hexagon.cpp" "CMakeFiles/MicroVectorEditor.dir/src/Hexagon.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/Hexagon.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/Line.cpp" "CMakeFiles/MicroVectorEditor.dir/src/Line.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/Line.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/Objects.cpp" "CMakeFiles/MicroVectorEditor.dir/src/Objects.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/Objects.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/Rectangle.cpp" "CMakeFiles/MicroVectorEditor.dir/src/Rectangle.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/Rectangle.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/Text.cpp" "CMakeFiles/MicroVectorEditor.dir/src/Text.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/Text.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/getAttributeValue.cpp" "CMakeFiles/MicroVectorEditor.dir/src/getAttributeValue.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/getAttributeValue.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/main.cpp" "CMakeFiles/MicroVectorEditor.dir/src/main.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/main.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/mainwindow.cpp" "CMakeFiles/MicroVectorEditor.dir/src/mainwindow.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/mainwindow.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/mouseMoveEvent.cpp" "CMakeFiles/MicroVectorEditor.dir/src/mouseMoveEvent.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/mouseMoveEvent.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/mousePressEvent.cpp" "CMakeFiles/MicroVectorEditor.dir/src/mousePressEvent.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/mousePressEvent.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/mouseReleaseEvent.cpp" "CMakeFiles/MicroVectorEditor.dir/src/mouseReleaseEvent.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/mouseReleaseEvent.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/redo.cpp" "CMakeFiles/MicroVectorEditor.dir/src/redo.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/redo.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/roundedrectangle.cpp" "CMakeFiles/MicroVectorEditor.dir/src/roundedrectangle.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/roundedrectangle.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/setupMenus.cpp" "CMakeFiles/MicroVectorEditor.dir/src/setupMenus.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/setupMenus.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/setupToolBar.cpp" "CMakeFiles/MicroVectorEditor.dir/src/setupToolBar.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/setupToolBar.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/svgparser.cpp" "CMakeFiles/MicroVectorEditor.dir/src/svgparser.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/svgparser.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/toInt.cpp" "CMakeFiles/MicroVectorEditor.dir/src/toInt.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/toInt.cpp.o.d"
+  "/Users/ritikraj/Desktop/Micro-Vector-Editor/src/undo.cpp" "CMakeFiles/MicroVectorEditor.dir/src/undo.cpp.o" "gcc" "CMakeFiles/MicroVectorEditor.dir/src/undo.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
